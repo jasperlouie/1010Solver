79db975a25c966f5d88eef81edad4a97eea23158
Jasper Louie <jlouie95@gmail.com>
2015-08-04 02:08:11 -0700

    finished piece implementation, started command line ui

--- 1010.py
+++ 1010.py
@@ -1,3 +1,5 @@
+import random
+
 class Board:
     """A 10 x 10 list of 0's and 1's, 0 means a space is clear, 1 means it's occupied. Represents a gamestate of the 10 x 10 board
     On this board, the coordinates system starts with (0,0) being the top left corner, and the first coordinate being the x, and the second being the y."""
@@ -55,17 +57,19 @@ class Board:
         else:
             raise InvalidMoveException('this is not a valid move')
 
-    def __str__():
-        out = '-' * 12 + '\n'
+    def __str__(self):
+        
         for y in range(10):
+            out = '-' * 21 + '\n'
             out += '|'
             for x in range(10):
                 if self.matrix[x][y] == 0:
                     out += ' '
                 else:
                     out += 'X'
-            out += '|\n'
-        out += '-' * 12
+                out+= '|'
+            out += '\n'
+        out += '-' * 21
         return out
 
 class Piece:
@@ -73,4 +77,69 @@ class Piece:
     in respect to the left most block in the top row of a piece. This block is represented by (0,0)."""
     def __init__(self, blocks = [(0,0)]):
         self.blocks = blocks
+
+    def __str__(self):
+        out = ""
+        x_offset = 0
+        y_offset = 0
+        width = 0
+        height = 0
+        for block in self.blocks:
+            width = max(width, block[0]+1)
+            height = max(height, block[1]+1)
+            x_offset = max(x_offset,-block[0])
+            y_offset = max(y_offset,-block[1])
+        width += x_offset
+        height += y_offset
+        for y in range(height):
+            for x in range(width):
+                if (x-x_offset,y-y_offset) in self.blocks:
+                    out += 'X'
+                else:
+                    out += ' '
+            out += '\n'
+        return out
+
+
     # as an example, a piece that is three blocks horizontally would be [(0,0),(1,0),(2,0)]
+
+# Starting game loop
+############################################################################
+game_over = False
+piece_list = [
+Piece(), #singleton
+Piece([(0,0),(1,0)]), #2 horizontal
+Piece([(0,0),(1,0),(2,0)]), #3 horizontal
+Piece([(0,0),(1,0),(2,0),(3,0)]), #4 horizontal
+Piece([(0,0),(1,0),(2,0),(3,0),(4,0)]), #5 horizontal
+Piece([(0,0),(0,1)]), #2 vertical
+Piece([(0,0),(0,1),(0,2)]), #3 vertical
+Piece([(0,0),(0,1),(0,2),(0,3)]), #4 vertical
+Piece([(0,0),(0,1),(0,2),(0,3),(0,4)]), #5 vertical
+Piece([(0,0),(0,1),(1,1)]), #short L
+Piece([(0,0),(0,1),(-1,1)]), #short L mirrored
+Piece([(0,0),(0,1),(1,0)]), #short L flipped
+Piece([(0,0),(1,1),(1,0)]), #short L mirrored flipped
+Piece([(0,0),(0,1),(0,2),(1,2),(2,2)]), #Long L
+Piece([(0,0),(0,1),(0,2),(-1,2),(-2,2)]), #Long L mirrored
+Piece([(0,0),(0,1),(0,2),(1,0),(2,0)]), #short L flipped
+Piece([(0,0),(2,1),(2,2),(1,0),(2,0)]), #short L mirrored flipped
+Piece([(0,0),(0,1),(1,0),(1,1)]), #2x2
+Piece([(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]) #3x3
+]
+
+count = 0
+current_pieces = []
+while not game_over:
+    count += 1
+    if count > 100:
+        break
+    print ("Piece Set {}".format(count))
+    for num in range(3):
+        current_pieces.append(random.choice(piece_list))
+    while current_pieces != []:
+        print("These are the current pieces")
+        for i in range(len(current_pieces)):
+            print ("{})".format(i+1))
+            print (current_pieces[i])
+        
