49063b265ebf36d487a0e58c68b9defcdc763d58
Jasper Louie <jlouie95@gmail.com>
2015-08-06 01:15:36 -0700

    Completely finished game functionality

--- 1010.py
+++ 1010.py
@@ -1,21 +1,83 @@
 import random
 
+class InvalidMoveException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+class Piece:
+    """A list of tuples that contain the coordinates of the blocks in a piece,
+    in respect to the left most block in the top row of a piece. This block is represented by (0,0)."""
+    def __init__(self, blocks = [(0,0)]):
+        self.blocks = blocks
+    # as an example, a piece that is three blocks horizontally would be [(0,0),(1,0),(2,0)]
+
+    def __str__(self):
+        out = ""
+        x_offset = 0
+        y_offset = 0
+        width = 0
+        height = 0
+        for block in self.blocks:
+            width = max(width, block[0]+1)
+            height = max(height, block[1]+1)
+            x_offset = max(x_offset,-block[0])
+            y_offset = max(y_offset,-block[1])
+        width += x_offset
+        height += y_offset
+        for y in range(height):
+            for x in range(width):
+                if (x-x_offset,y-y_offset) in self.blocks:
+                    out += 'X'
+                else:
+                    out += ' '
+            out += '\n'
+        return out
+
+class Move:
+    def __init__(self, piece, x, y):
+        self.piece = piece
+        self.x = x
+        self.y = y
+
+def convert_str_to_move(string, current_pieces):
+    move_list = string.replace(' ', '').split(',')
+    return Move(current_pieces[int(move_list[0])-1], int(move_list[1]), int(move_list[2]))
+
+
 class Board:
     """A 10 x 10 list of 0's and 1's, 0 means a space is clear, 1 means it's occupied. Represents a gamestate of the 10 x 10 board
     On this board, the coordinates system starts with (0,0) being the top left corner, and the first coordinate being the x, and the second being the y."""
-    EMPTY_BOARD = [[0] * 10 for x in range(10)] 
+    EMPTY_BOARD = [[0] * 10 for x in range(10)]
 
     """Constructs a Board. Default is empty"""
-    def __init__(self, matrix = EMPTY_BOARD):
+    def __init__(self, matrix = EMPTY_BOARD, current_pieces = []):
         self.matrix = matrix
+        self.current_pieces = []
 
     """Returns True if you can place Piece p at the Location loc"""
-    def is_valid_move(self, p, loc):
-        for block in p.blocks:
-            currX, currY = block[0] + loc[0], block[1] + loc[1]
+    def is_valid_move(self, move):
+        for block in move.piece.blocks:
+            currX, currY = block[0] + move.x, block[1] + move.y
             if currX not in range(10) or currY not in range(10) or self.matrix[currX][currY] == 1:
                 return False
         return True
+
+    def get_valid_moves(self):
+        out = []
+        for piece in self.current_pieces:
+            for x in range(10):
+                for y in range(10):
+                    move = Move(piece, x, y)
+                    if self.is_valid_move(move):
+                        out.append(move)
+        return out
+
+    def has_valid_moves(self):
+        if self.get_valid_moves() == []:
+            return False
+        return True
+
 
     """Returns a list of numbers containing the numbers of the rows that are currently full in the board"""
     def get_full_rows(self):
@@ -41,27 +103,26 @@ class Board:
 
     """Places a piece p at a space loc (given by a tuple with two coordinates), updates board accordingly
         Returns a tuple of two lists that contains rows and cols cleared respectively"""
-    def place_piece(self, move, current_pieces):
-        p = current_pieces[move.piece_index]
-        loc = (move.x, move.y)
-        if self.is_valid_move(p , loc):
+    def place_piece(self, move):
+        if self.is_valid_move(move):
             # placing piece
-            for block in p.blocks:
-                self.matrix[block[0] + loc[0]][block[1] + loc[1]] = 1
+            for block in move.piece.blocks:
+                self.matrix[block[0] + move.x][block[1] + move.y] = 1
             # clearing appropriate rows/cols
             full_rows, full_cols = self.get_full_rows(), self.get_full_cols()
             for y in full_rows:
-                # print("clearing row {}!".format(y))
                 for x in range(10):
                     self.matrix[x][y] = 0
             for x in full_cols:
-                # print("clearing column {}!".format(x))
                 for y in range(10):
                     self.matrix[x][y] = 0
-            current_pieces.pop(move.piece_index)
+            self.current_pieces.remove(move.piece)
             return full_rows, full_cols
         else:
             raise InvalidMoveException('this is not a valid move')
+    def refresh_pieces(self):
+        for num in range(3):
+            self.current_pieces.append(random.choice(piece_list))
 
     def __str__(self):
         out = "  "
@@ -80,57 +141,11 @@ class Board:
             out += '\n'
         out += ' '+'-' * 41
         return out
-class InvalidMoveException(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-class Piece:
-    """A list of tuples that contain the coordinates of the blocks in a piece,
-    in respect to the left most block in the top row of a piece. This block is represented by (0,0)."""
-    def __init__(self, blocks = [(0,0)]):
-        self.blocks = blocks
-
-    def __str__(self):
-        out = ""
-        x_offset = 0
-        y_offset = 0
-        width = 0
-        height = 0
-        for block in self.blocks:
-            width = max(width, block[0]+1)
-            height = max(height, block[1]+1)
-            x_offset = max(x_offset,-block[0])
-            y_offset = max(y_offset,-block[1])
-        width += x_offset
-        height += y_offset
-        for y in range(height):
-            for x in range(width):
-                if (x-x_offset,y-y_offset) in self.blocks:
-                    out += 'X'
-                else:
-                    out += ' '
-            out += '\n'
-        return out
-
-class Move:
-    def __init__(self, piece_index, x, y):
-        self.piece_index = piece_index
-        self.x = x
-        self.y = y
-
-    def __init__(self, string):
-        move_list = string.replace(' ', '').split(',')
-        self.piece_index = int(move_list[0])-1
-        self.x = int(move_list[1])
-        self.y = int(move_list[2])
 
 
-    # as an example, a piece that is three blocks horizontally would be [(0,0),(1,0),(2,0)]
 
-# Starting game loop
-############################################################################
 
+# All possible pieces
 piece_list = [
 Piece(), #singleton
 Piece([(0,0),(1,0)]), #2 horizontal
@@ -151,35 +166,51 @@ Piece([(0,0),(0,1),(0,2),(1,0),(2,0)]), #short L flipped
 Piece([(0,0),(2,1),(2,2),(1,0),(2,0)]), #short L mirrored flipped
 Piece([(0,0),(0,1),(1,0),(1,1)]), #2x2
 Piece([(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]) #3x3
-]
+] 
+
+# Starting game loop
+############################################################################
+
 def play(get_move, verbose = True):
     game_over = False
-    move_num = 0
-    current_pieces = []
+    move_num = 1
+    cleared_lines = 0
     board = Board()
-    while not game_over:
-        # print ("Piece Set {}".format(count))
-        for num in range(3):
-            current_pieces.append(random.choice(piece_list))
-        while current_pieces != []:
-            if verbose:
-                print ("Here is the current board: \n"+str(board))
-                print("These are the current pieces")
-                for i in range(len(current_pieces)):
-                    print ("{})".format(i+1))
-                    print (current_pieces[i])
-            move = get_move(board, current_pieces)
-            try:
-                board.place_piece(move, current_pieces)
-                print("Placed piece {} at {},{}".format(move.piece_index,move.x,move.y))
-            except InvalidMoveException:
-                print("That is an invalid move, please make sure you are placing the piece in a valid space")
-            except:
-                print("Please enter a valid piece number, enter the correct format for a move: piece_num, x, y")
+    board.refresh_pieces()
+    while board.has_valid_moves():
 
-def human(board, current_pieces):
+        if verbose:
+            print("##########################################")
+            print ("Move {}:".format(move_num))
+            print ("Here is the current board: \n"+str(board))
+            print("These are the current pieces")
+            for i in range(len(board.current_pieces)):
+                print ("{})".format(i+1))
+                print (board.current_pieces[i])
+        move = get_move(board, board.current_pieces)
+        try:
+            cleared_rows, cleared_cols = board.place_piece(move)
+            print("------------------------------------------")
+            print("{}placed at {},{}".format(move.piece,move.x,move.y))
+            if cleared_cols != []:
+                for col in cleared_cols:
+                    print("Cleared Column {}!".format(col))
+            if cleared_rows != []:
+                for row in cleared_rows:
+                    print("Cleared Row {}!".format(row))
+            cleared_lines += len(cleared_rows) + len(cleared_cols)
+            move_num += 1
+        except InvalidMoveException:
+            print("That is an invalid move, please make sure you are placing the piece in a valid space")
+        except:
+            print("Please enter a valid piece number, enter the correct format for a move: piece_num, x, y")
+        if board.current_pieces == []:
+            board.refresh_pieces()
+    return move_num, cleared_lines
+
+def human_input(board, current_pieces):
     move_input = input("Please input your move: ")
-    # print(repr(move_input)
-    return Move(move_input)
+    return convert_str_to_move(move_input, current_pieces)
 
-play(human)
+score = play(human_input)
+print("Game Over! You lasted {} moves and cleared {} lines!".format(score[0],score[1]))
